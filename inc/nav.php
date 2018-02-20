dc4f20da7b9decff8ba25b6b4d32ed0b38a0c3e6
Santi <santiwebdigitals@gmail.com>
2018-02-20 09:58:20 +0100

    centrado

--- inc/nav.php
+++ inc/nav.php
@@ -1,11 +1,12 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light " style="align-center">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark nav justify-content-center">
+	  			<div class="nav justify-content-center">
 	  			<a class="navbar-brand" href="index.php">
-	  				<span class="navbar-brand mb-0 h1">Inicio</span>
+	  				<span class="navbar-brand mb-0 h1 ">Inicio</span>
 	  			</a>
 	  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    			<span class="navbar-toggler-icon"></span>
 	  			</button>
-	  			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	  			<div class="collapse navbar-collapse " id="navbarSupportedContent">
 	    			<ul class="navbar-nav mr-auto">
 	      			<!-- 	<li class="nav-item dropdown">
 	        				<a class="nav-link active dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Desplegable 1</a>
@@ -32,4 +33,5 @@
 			      		</li>
 	    			</ul>
 	  			</div>
+	  		</div>
 </nav>
