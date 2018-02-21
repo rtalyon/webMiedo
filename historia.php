5b658f3b4b1270bf131dd936831cc73d20bf77d7
rtalyon <rta.lyon@gmail.com>
2018-02-21 12:35:11 -0800

    cambios

--- historia.php
+++ historia.php
@@ -2,10 +2,10 @@
 	<div id="content" class="container-fluid ">
 		<div class="row mt-5">
 			<div class="col-2 offset-1">
-				<img src="img/Enriqueta-Marti (1).jpg" class="img-fluid mx-5">
+				<img src="img/Enriqueta-Marti (1).jpg" class="img-fluid">
 			</div>	
 			<div>
-				<a href="HistoriasCortas.php" target="_blank"><p class="mt-5 font-weight-bold text-white" class="fear">Histtia del vampido de muucho, Enriqueta Martí Ripollés</p></a>
+				<a href="HistoriasCortas.php" target="_blank"><p class="mt-5 font-weight-bold text-white" class="fear">Historia de la vampira del Raval, Enriqueta Martí Ripollés</p></a>
 			</div>
 		</div>
 	</div>
