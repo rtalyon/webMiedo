52ed7874a6da96073c49e141c917575d0430d9ca
rtalyon <rta.lyon@gmail.com>
2018-02-22 10:23:52 -0800

    klasdf

--- inc/head.php
+++ inc/head.php
@@ -7,9 +7,11 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Butcherman" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet">
 	</head>
 	</body>
+		<a name="came"></a>
 		<?php include("header.php");?>
