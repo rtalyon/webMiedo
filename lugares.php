f0a0c5fe33853c03c761496ab7dfe82e33177781
Santi <santiwebdigitals@gmail.com>
2018-02-22 14:30:06 +0100

    sdsadasdasd

--- lugares.php
+++ lugares.php
@@ -1,8 +1,8 @@
 <?php include("inc/head.php");?>
 	<div id="content" class="container-fluid ">
 		<div class="row mt-5">
-			<div class="col-2 offset-1" id="imghos">
-				<img src="img/hospital.jpeg" alt="hospital">
+			<div class="col-3 offset-1" id="imghos">
+				<img src="img/hospital.jpeg" alt="hospital" class="img-fluid">
 			</div>	
 			<div>
 				<a href="torax.php" target="_blank"><p class="mt-5 font-weight-bold text-white" class="place">Hospital del Torax</p></a>
